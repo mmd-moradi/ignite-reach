385f75e16a575261bea4bae012537758dc836cbb
Mohammad Moradi <110211422+mmd-moradi@users.noreply.github.com>
2024-05-31 12:02:46 -0300

    Update index.tsx

--- src/components/shared/interface/layout/Footer/index.tsx
+++ src/components/shared/interface/layout/Footer/index.tsx
@@ -37,7 +37,7 @@ const Footer = ({config}: Props) => {
               href="/"
               passHref
             >
-              <div className="relative w-[146px] h-[44px]">
+              <div className="relative w-[194px] h-[44px]">
                 <Image
                   alt={config.footerLogo.alt}
                   src={urlFor(config.footerLogo).url()}
